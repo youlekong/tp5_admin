ddf2440a40fcfe0f6128d582f6db00e9b0876cfc
koyole <424188487@qq.com>
2018-01-29 13:19:52 +0800

    add lang.php 配置文件

---
+++ app/admin/lang/zh-cn.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    // 服务器信息
+    'server_info' => '服务器信息',
+    'php_version' => 'php版本',
+    'env' => '运行环境',
+    'is_debug' => '开启调试',
+    'server' => '服务器',
+    'timezone' => '时区',
+
+    // 系统信息
+    'system' => '系统',
+    'ip_addr' => 'ip地址',
+    'browser' => '浏览器',
+    'city' => '城市'
+];
